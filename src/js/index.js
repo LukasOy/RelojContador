648fa9f6c35d73c48f551070ff47ff2fcadbf93c
LukasOy <lukas.oylu@gmail.com>
2022-11-16 00:35:47 +0000

    primer guardado

--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,26 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+function Contador(props){
+
+    return (<div className="ContadorPrincipal">
+            <div className="relojito" ><i class="fa-solid fa-clock"></i></div>
+            <div className="Primercontador"></div>
+            <div className="Segundocontador"></div>
+            <div className="Tercercontador"></div>
+            <div className="Cuartocontador"></div> 
+            <div className="Quintocontador"></div>
+            </div>)
+}
+
+let tiempo =  0;
+
+setInterval(function numeroscontados(props){
+    return (<div className ="counter"> <h1>Hello World</h1></div>)
+    tiempo++
+},500)
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Contador />, document.querySelector("#app"));
+
+
