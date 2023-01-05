bee4d49bd717c0654ac872ed6a848e221b0b7695
LukasOy <lukas.oylu@gmail.com>
2023-01-05 02:10:50 +0000

    123

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,21 +11,17 @@ const Home = ({segundos}) => {
 	let centena = string[string.length -3]
 	let dmil = string[string.length -4]
 
-	
-	function stopCounter() {
-		// aquí debes poner el código para detener el contador
-	  }
 	return (
 		
 		<div className="Reloj1">
 			<img src="https://pbs.twimg.com/media/CDfNmCAUUAE_4fO.jpg" alt="estas muerto.com"></img>
 		<div className="text-center">
-			<button className="btn btn-danger" >¿Cuanto Falta? </button>
+			<button className="btn btn-danger" >¿Cuanto llevas? </button>
 			<button className="btn btn-danger" > {dmil==undefined? "0":centena} </button>
 			<button className="btn btn-danger" > {centena==undefined? "0":centena} </button>
 			<button className="btn btn-danger" > {decena==undefined? "0":decena} </button>
 			<button className="btn btn-danger" > {unidad} </button>
-			<button className="btn btn-danger" onClick={stopCounter}>ESTAP</button>
+			
 		</div>
 		</div>
 	);
